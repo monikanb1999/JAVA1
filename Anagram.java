5f148464fad2a1c04782312410ea28916dca4dc9
monikanb1999 <73658104+monikanb1999@users.noreply.github.com>
2020-10-31 19:24:34 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,27 +1,35 @@
-import java.util.Scanner;
+//Anagram 
+//To check whether two given strings are anagram of each other or not. 
+//For example, “abcd” and “dabc” 
+import java.util.*;
+import java.io.*;
 class Anagram{
+        public static boolean isAnagram(String a,String b)
+        {    //remove whitespace
+             //remove lowercase
+             //make it to chararray
+             //sorting array                
+              a = a.replace(" ","").toLowerCase();
+              b = b.replace(" ","").ToLowerCase();
+                
+                char[] c= a.toCharArray();
+                char[] d= b.toCharArray();
+                
+                Arrays.sort(c);
+                Arrays.sort(d);
+                if(Arrays.equals(c,d))
+                     return true;
+                else 
+                     return false;
+        }                         
 public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        //to get input from user
         String a = scan.next();
         String b = scan.next();
-        scan.close();
-      a = a.toLowerCase();
-      b = b.toLowerCase();
-      int arr[] = new int[26];
-      for(int i=0;i<a.length();i++)
-      {
-        arr[a.charAt(i)-97]++;
-      }
-      for(int j=0;j<b.length();j++)
-      {
-        arr[b.charAt(j)-97]--;
-      }
-      for(int k=0;k<arr.length;k++)
-      {
-        if(k != 0)
-        System.out.println("No");
-      }
-      System.out.println("Yes");
-    }
-  }
+        boolean ret =isAnagram(a,b);
+        System.out.println((ret)?"Anagram":"not Anagram");
+}
+}
+     
